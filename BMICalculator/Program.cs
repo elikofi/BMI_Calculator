43f9082ff6add5bcb19db12d18b936a3fc9c1b78
elijah amoako <elijahamoako72@gmail.com>
2023-01-24 21:52:28 +0000

    first commit

---
+++ BMICalculator/Program.cs
@@ -0,0 +1,117 @@
+﻿Console.WriteLine("Welcome to the BMI calculator application.");
+Console.WriteLine("__________________________________________\n");
+
+Console.WriteLine("Which version would you like to use : \n");
+Console.WriteLine("1. Standard");
+Console.WriteLine("2. Metric\n");
+double weight, height, heightInInches, IBMI = 0;
+decimal BMI;
+string versionType = Console.ReadLine();
+
+switch (versionType)
+{
+    case "1":
+        Console.Write("Enter your weight in pounds (lbs): - ");
+        weight = double.Parse(Console.ReadLine());
+
+        Console.Write("\nEnter your height in feet: - ");
+        height = double.Parse(Console.ReadLine());
+
+        heightInInches = (int)(height * 12);
+
+        IBMI = (weight / Math.Pow(heightInInches, 2)) * 703;
+
+        switch (IBMI)
+        {
+            case < 18.5:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Under Weight.");
+
+                break;
+
+            case < 25.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Healthy Weight.");
+                break;
+
+            case < 30.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: OverWeight.");
+
+                break;
+
+            case >= 30.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Obesity.");
+
+                break;
+            default:
+                Console.WriteLine("Invalid input.");
+                break;
+        }
+
+        Console.WriteLine();
+        break;
+
+    // second case: measurement for metric version
+    case "2":
+
+        Console.Write("Enter your weight in kilograms (kg): - ");
+        weight = double.Parse(Console.ReadLine());
+
+        Console.Write("\nEnter your height in metres: - ");
+        height = double.Parse(Console.ReadLine());
+
+        IBMI = weight / Math.Pow(height, 2);
+
+        switch (IBMI)
+        {
+            case < 18.5:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Under Weight.");
+
+                break;
+
+            case < 25.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Healthy Weight.");
+                break;
+
+            case < 30.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: OverWeight.");
+
+                break;
+
+            case >= 30.0:
+                BMI = Convert.ToDecimal(IBMI);
+                BMI = Math.Round(BMI, 2);
+                Console.WriteLine($"\nYour Body Mass Index (BMI) is; {BMI}\n");
+                Console.WriteLine("Weight Status: Obesity.");
+
+                break;
+            default:
+                Console.WriteLine("Invalid input.");
+                break;
+        }
+        break;
+
+    default:
+        Console.WriteLine("Invalid input!");
+        break;
+}
+Console.ReadLine();
